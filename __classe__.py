ccc6569729cf4a674628f7401eed4348c974c917
Lilian <35975697+Nalil10@users.noreply.github.com>
2019-12-03 21:37:36 +0100

    Update __classe__.py

--- __classe__.py
+++ __classe__.py
@@ -1,4 +1,4 @@
-from turtle import up, goto, down, home
+import turtle
 
 class Point:
     def __init__(self,name = "", x= 0, y= 0):
@@ -8,10 +8,18 @@ class Point:
         self.name = name
         self.x = x
         self.y = y
+        fichier = open("data.txt", 'a')
+        fichier.write(str(self.name), ": (", str(self.x), ";", ")\n")
+        fichier.close()
     def tracer(self):
         """
         Trace le point dans le repère
         """
-        up()
-        goto(self.x, self.y)
-        down()
+        turtle.up()
+        turtle.goto(self.x, self.y)
+        turtle.dot(3)
+        turtle.write(str(self.name), "\n", "(", str(self.x), ";", str(self.y), ")")
+        turtle.down()
+
+if __name__ == "__main__":
+    a = Point("A", 50, 50)
