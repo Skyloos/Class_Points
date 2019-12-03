8dac89a10871d710a326d48b911ed4f9f7f42c42
Lilian <35975697+Nalil10@users.noreply.github.com>
2019-12-03 22:21:38 +0100

    Update __classe__.py

--- __classe__.py
+++ __classe__.py
@@ -3,6 +3,7 @@ Ensemble des class utilisées dans __main__
 """
 import turtle as ttl
 import random as rd
+import __function__ as fonc
 
 class Point:
     def __init__(self,name = "", x= 0, y= 0):
@@ -26,13 +27,15 @@ class Point:
         """
         Trace le point dans le repère
         """
-        ttl.up()
-        ttl.goto(self.x, self.y)
-        ttl.dot(3)
-        ttl.write(str(self.name), "\n", "(", str(self.x), ";", str(self.y), ")")
-        ttl.down()
+        tortue.up()
+        tortue.goto(self.x, self.y)
+        tortue.dot(3)
+        tortue.write(str(self.name)+ "\n" + "(", str(self.x) + ";", str(self.y) + ")")
+        tortue.down()
 
 if __name__ == "__main__":
     print("Lancement du module __classe__ en cours...")
+    tortue = fonc.tortue()
+    fonc.repere(tortue)
     a = Point("A", 50, 50)
     print("Fin du module.")
