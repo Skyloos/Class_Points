9d4d8d5a7c927812b36a080b481e793eb5c4c64c
Lilian <35975697+Nalil10@users.noreply.github.com>
2019-12-03 21:37:40 +0100

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,10 +1,10 @@
-from turtle import up, goto, down, home
 from time import sleep
-from __function__ import *
-from __classe__ import *
-        
-repere()
-a = Point("A", 50, 50)
+import __function__  as fonc
+import __classe__ as cl
+
+fonc.clear_data()
+fonc.repere()
+a = cl.Point("A", 50, 50)
 a.tracer()
 
 while 1:
