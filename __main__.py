dce5366ae1a7a3cd702036232f1f8c9b52d822d7
Lilian <35975697+Nalil10@users.noreply.github.com>
2019-12-03 21:17:51 +0100

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,24 +1,11 @@
 from turtle import up, goto, down, home
 from time import sleep
 from __function__ import *
-
-class Point:
-    def __init__(self,name = "", x= 0, y= 0):
-        """
-        Définit les caractéristiques du point dans "self"
-        """
-        self.name = name
-        self.x = x
-        self.y = y
-    def tracer(self):
-        """
-        Trace le point dans le repère
-        """
-        up()
-        goto(self.x, self.y)
-        down()
+from __classe__ import *
         
 repere()
+a = Point("A", 50, 50)
+a.tracer()
 
 while 1:
     sleep(1)
