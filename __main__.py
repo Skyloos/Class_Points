3bdbe9655022a548189d4e5a476a2ad0e8d3398c
Lilian <35975697+Nalil10@users.noreply.github.com>
2019-12-04 14:22:02 +0100

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -6,7 +6,7 @@ fonc.clear_data()
 fonc.repere(fonc.tortue())
 
 liste = []
-for point in range(1):
+for point in range(3):
     p = cl.Point()
     p.random()
     p.tracer()
@@ -16,7 +16,7 @@ if len(liste) == 1:
     liste[0].liage(liste)
     liste[0].milieu(liste)
 else:
-    liste[0].liage(liste[1:-1])
+    liste[0].liage(liste[1:], liste[-1])
     liste[0].milieu(liste[1:])
 
 while 1:
