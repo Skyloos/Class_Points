c9032c6e40e4aa27ec706cc5ca5e69f2ff42aa9d
Lilian <35975697+Nalil10@users.noreply.github.com>
2019-12-04 19:34:49 +0100

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -5,21 +5,23 @@ import os
 fonc.clear_data()
 fonc.repere(fonc.tortue())
 dictMilieu = fonc.dictionnary_milieu()
-"""
+
 liste = []
-for point in range(4):
+for point in range(100):
     p = cl.Point()
     p.random()
     p.tracer()
     liste.append(p)
-
+for element in range(len(liste)):
+    liste[element].liage(liste)
+"""
 if len(liste) == 1:
     liste[0].liage(liste)
     liste[0].milieu(liste)
 else:
     liste[0].liage(liste[1:len(liste)], liste[0])
     liste[0].milieu(liste[1:])
-"""
+
 a = cl.Point()
 a.random()
 a.tracer()
@@ -29,5 +31,6 @@ b.random()
 b.tracer()
 
 a.liage(b)
+"""
 while 1:
     os.system("pause")
