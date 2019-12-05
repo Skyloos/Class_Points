503c76adba813903d8b11c190eda74a2c402a85d
Lilian <35975697+Nalil10@users.noreply.github.com>
2019-12-05 07:47:37 +0100

    Update __classe__.py

--- __classe__.py
+++ __classe__.py
@@ -69,7 +69,7 @@ class Point:
                 fichier.write("[" + self.name + other[point].name + "] = " + str(segment.longueur()) + "\n")
             fichier.close()
         tortue.ht()
-    def milieu(self, *other):
+    def milieu(self, dictMilieu, *other):
         """
         Cree un point M qui est le milieu de plusieurs points
         """
@@ -93,7 +93,7 @@ class Point:
         Milieu_y /= (len(other)+1)
         Milieu_name += "]"
         Milieu = Point(Milieu_name, Milieu_x, Milieu_y)
-        #dictMilieu[str(Milieu_name)] = Milieu
+        dictMilieu[str(Milieu_name)] = Milieu
         Milieu.tracer()
         return Milieu 
 
