b31282f8e0e41836f4632a06aef7d4588ade8866
Lilian <35975697+Nalil10@users.noreply.github.com>
2019-12-07 12:49:08 +0100

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,26 +1,28 @@
 import __function__  as fonc
 import __classe__ as cl
 import os
+import random as rd
 
 fonc.clear_data()
 fonc.repere(fonc.tortue())
 dictPoint = fonc.dictionnary_point()
-nameList = fonc.liste_name(3)
+nameList = fonc.liste_name(4)
 
 liste = []
-for point in range(3):
-    p = cl.Point(dictPoint)
-    p.random(nameList, dictPoint)
+for point in range(4):
+    print(nameList)
+    p = cl.Point(dictPoint, nameList[rd.randint(0,4-point)], 100*point, 100*point)
+    #p.random(nameList, dictPoint)
     p.tracer("N")
     liste.append(p)
 
 
 if len(liste) == 1:
     liste[0].liage(liste)
-    liste[0].barycentre(dictPoint, "N", liste)
+    liste[0].milieu(dictPoint, "N", liste)
 else:
     liste[0].liage(liste[1:len(liste)], liste[0])
-    liste[0].barycentre(dictPoint, "N", liste[1:])
+    liste[0].milieu(dictPoint, "O", liste[1:])
 
 
 print(dictPoint)
