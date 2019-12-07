7b4e4ec25d9cbf5391a4d0b3a5db4c1c623c2e95
Lilian <35975697+Nalil10@users.noreply.github.com>
2019-12-07 20:01:17 +0100

    Update interpretor.py

---
+++ interpretor.py
@@ -0,0 +1,14 @@
+import math
+
+u=(5,3)
+v=(10,5)
+
+def norme(u):
+    """"""
+    return math.sqrt(u[0]**2+u[1]**2)
+
+def angle():
+    """"""
+    return angle
+
+ps=norme(u)*norme(v)*math.cos(angle(u,v))
