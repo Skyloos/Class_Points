6c0503d4953ccdaf8ed714b40ed5246869645c12
Lilian <35975697+Nalil10@users.noreply.github.com>
2019-12-09 21:46:57 +0100

    Update __Fonction__.py

--- __Fonction__.py
+++ __Fonction__.py
@@ -12,8 +12,7 @@ class Fonction:
                 chaine += "(" + str(self.para[element]) + ")*x**" + str(element) + "+"
             else:
                 chaine += str(self.para[element]) + "*x**" + str(element) + "+"
-        chaine = chaine[:-1]
-        self.chaine = chaine
+        self.chaine = chaine[:-1]
     def __str__(self):
         chaine = ""
         for element in range(len(self.para)):
@@ -26,15 +25,20 @@ class Fonction:
         chaine = chaine[:-1]
         return chaine
     def y(self,x):
-        chaine = ""
+        if x < 0:
+            x = "(" + str(x) + ")"
         return eval(self.chaine.replace("x", str(x)))
     def maxi(self):
-        if "x**2" in 
+        if "x**2" in self.chaine:
+            alpha = (-self.para[1])/(2*self.para[2])
+            strAlpha = "(" + str(alpha) + ")"
+            beta = eval(self.chaine.replace("x", strAlpha))
+            return (alpha, beta)
 
 
 if __name__ == "__main__":
     print("Lancement du module __Fonction__ en cours...")
-    f = Fonction(0,-1,1)
-    print(f)
-    print(f.y(5))
+    f = Fonction(0,1,1)
+    print(f.maxi()[0])
+    print(f.y(f.maxi()[0]))
     print("Fin du module.")
