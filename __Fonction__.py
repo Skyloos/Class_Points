be246c6cc4afeac243c9671c5035781d02fcf90b
Lilian <35975697+Nalil10@users.noreply.github.com>
2019-12-09 21:09:47 +0100

    Update __Fonction__.py

--- __Fonction__.py
+++ __Fonction__.py
@@ -1,3 +1,17 @@
 class Fonction:
-    def __init__(self, *other):
-        self.other = other
+    def __init__(self, *para):
+        """
+        """
+        self.para = para
+    def __str__(self):
+        chaine = ""
+        for element in range(len(para)):
+            if element == 0:
+                chaine += str(para[element])
+            else:
+                chaine += str(para[element]) + "**" + str(element) + "+"
+        chaine = chaine[:-1]
+
+if __name__ == "__main__":
+    print("Lancement du module __Fonction__ en cours...")
+    print("Fin du module.")
