dece7eec12153aa59fb99bef06d6ed0fd3691ddc
Lilian <35975697+Nalil10@users.noreply.github.com>
2019-12-09 22:00:50 +0100

    Update __Fonction__.py

--- __Fonction__.py
+++ __Fonction__.py
@@ -1,3 +1,8 @@
+import __Point__ as cp
+import __function__ as fonc
+import turtle as ttl
+
+
 class Fonction:
     def __init__(self, *para):
         """
@@ -28,17 +33,23 @@ class Fonction:
         if x < 0:
             x = "(" + str(x) + ")"
         return eval(self.chaine.replace("x", str(x)))
-    def maxi(self):
+    def extremum(self):
         if "x**2" in self.chaine:
             alpha = (-self.para[1])/(2*self.para[2])
             strAlpha = "(" + str(alpha) + ")"
             beta = eval(self.chaine.replace("x", strAlpha))
             return (alpha, beta)
+    def tracage(self, nameList):
+        if len(self.para) == 1:
+            tortue = fonc.turtue()
+            tortue.up()
+            tortue.goto(self.para[0], self.y(self.para[0]))
+            tortue.down()
+            tortue.ht()
 
 
 if __name__ == "__main__":
     print("Lancement du module __Fonction__ en cours...")
     f = Fonction(0,1,1)
-    print(f.maxi()[0])
-    print(f.y(f.maxi()[0]))
+    print(f.y(f.extremum()[0]))
     print("Fin du module.")
