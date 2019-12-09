bf8bfa7d02f5524c4bfb841f0c2f60ec656040f6
Lilian <35975697+Nalil10@users.noreply.github.com>
2019-12-09 21:27:01 +0100

    Update __Fonction__.py

--- __Fonction__.py
+++ __Fonction__.py
@@ -15,11 +15,20 @@ class Fonction:
         chaine = chaine[:-1]
         return chaine
     def y(self,x):
-        return eval(f.replace("x", str(x)))
+        chaine = ""
+        for element in range(len(self.para)):
+            if element == 0:
+                chaine += str(self.para[element]) + "+"
+            elif self.para[element] < 0:
+                chaine += "(" + str(self.para[element]) + ")*x**" + str(element) + "+"
+            else:
+                chaine += str(self.para[element]) + "*x**" + str(element) + "+"
+        chaine = chaine[:-1] 
+        return eval(chaine.replace("x", str(x)))
 
 
 if __name__ == "__main__":
     print("Lancement du module __Fonction__ en cours...")
     f = Fonction(0,-1,1)
-    print(f)
+    print(f.y(5))
     print("Fin du module.")
