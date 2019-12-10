9ce5a009134930a1466408cf154a7e3915ed0ece
Lilian <35975697+Nalil10@users.noreply.github.com>
2019-12-10 10:25:47 +0100

    Update __Fonction__.py

--- __Fonction__.py
+++ __Fonction__.py
@@ -1,6 +1,6 @@
 import __Point__ as cp
 import __function__ as fonc
-import turtle as ttl
+#import turtle as ttl
 import os
 
 
@@ -10,23 +10,26 @@ class Fonction:
 
         chaine = ""
         for element in range(len(self.coef)):
+            element -= len(self.coef) - 1
             if element == 0:
-                chaine += str(self.coef[element]) + "+"
+                chaine += str(self.coef[element-1])
             elif self.coef[element] < 0:
-                chaine += "(" + str(self.coef[element]) + ")*x**" + str(element) + "+"
+                chaine += "(" + str(self.coef[element-1]) + ")*x**" + str(element*(-1)) + "+"
             else:
-                chaine += str(self.coef[element]) + "*x**" + str(element) + "+"
-        self.chaine = chaine[:-1]
+                chaine += str(self.coef[element-1]) + "*x**" + str(element*(-1)) + "+"
+        self.chaine = chaine
     def __str__(self):
         chaine = ""
-        for element in range(len(self.coef)):
+        for element in range(-len(self.coef), 0):
+            i = element * (-1)
+            i -= 1
+            print(element)
             if element == 0:
-                chaine += str(self.coef[element]) + "+"
+                chaine += str(self.coef[element])
             elif self.coef[element] < 0:
                 chaine += "(" + str(self.coef[element]) + ")x**" + str(element) + "+"
             else:
                 chaine += str(self.coef[element]) + "x**" + str(element) + "+"
-        chaine = chaine[:-1]
         return chaine
     def y(self,x):
         """
@@ -34,6 +37,7 @@ class Fonction:
         """
         if x < 0:
             x = "(" + str(x) + ")"
+        print(self.chaine.replace("x", str(x)))
         return eval(self.chaine.replace("x", str(x)))
     def x(self,y):
         """
@@ -41,13 +45,13 @@ class Fonction:
         Non fini
         """
         if len(self.coef) == 2:
-            return eval("("+"(-" + str(self.coef[0]) +")+"+ "("+str(y)+")" + ")/("+ str(self.coef[1]) +")")
+            return eval("("+"(-" + str(self.coef[1]) +")+"+ "("+str(y)+")" + ")/("+ str(self.coef[0]) +")")
     def extremum(self):
         """
         Renvoie l'abscisse et l'ordonnee de l'extremum de la fonction
         """
         if "x**2" in self.chaine:
-            alpha = (-self.coef[1])/(2*self.coef[2])
+            alpha = (-self.coef[1])/(2*self.coef[0])
             strAlpha = "(" + str(alpha) + ")"
             beta = eval(self.chaine.replace("x", strAlpha))
             return (alpha, beta)
@@ -63,13 +67,23 @@ class Fonction:
             tortue.down()
             tortue.goto(180, self.y(180))
             tortue.ht()
+        else:
+            tortue = fonc.tortue()
+            tortue.up()
+            for i in range(-180,180):
+                tortue.down()
+                tortue.goto(i, self.y(i))
+                tortue.ht()
+            tortue.up()
+
 
 
 if __name__ == "__main__":
     print("Lancement du module __Fonction__ en cours...")
     fonc.repere(fonc.tortue())
-    f = Fonction(50,-2)
-    f.tracage()
+    f = Fonction(2,-2,0)
+    print(f)
+    #f.tracage()
     print("Fin du module.")
     while 1:
         os.system("pause")
